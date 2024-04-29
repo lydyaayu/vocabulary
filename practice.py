d55894fe8bd4ac54d00ae98088a90064cffed654
lydyaayu <lydyaayuferianti@gmail.com>
2024-04-29 20:15:11 +0700

    First Commit

---
+++ practice.py
@@ -0,0 +1,9 @@
+import requests
+
+api_key = 'f8777afc-be1c-41a2-bb78-045e19a79f8e'
+word = 'voluminous'
+root_url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
+final_url = f'{root_url}/{word}?key={api_key}'
+r = requests.get(final_url)
+result = r.json()
+print(result)
